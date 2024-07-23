9d474645ffb4e7a3f19e8d55886f6fe480b11590
Alvin Chan <altc.alchan@gmail.com>
2024-07-22 20:54:14 -0400

    fix: correct a few syntactical typos

--- bigint.c
+++ bigint.c
@@ -24,8 +24,8 @@ struct bigint bigint_add(const struct bigint *a, const struct bigint *b) {
 
 	int carry = 0;
 	for (int i = 0; i < a->ndigits; i++) {
-		int digit_sum = a + b + carry;
-		pushc(&sum, digit_sum % 10);
+		const int digit_sum = a->d[i] + b->d[i] + carry;
+		bigint_pushc(&sum, digit_sum % 10);
 		carry = digit_sum / 10;
 	}
 
