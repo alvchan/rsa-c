620ef3af3d31f1f930579419a7b6890db6c5ed0e
Alvin Chan <altc.alchan@gmail.com>
2024-07-23 15:09:55 -0400

    feat: add all current function signatures to header

--- bigint.h
+++ bigint.h
@@ -3,3 +3,10 @@ struct bigint {
 	int *d;
 };
 
+struct bigint *bigint_init(const int ndigits);
+void bigint_free(struct bigint *bi);
+void bigint_pushc(struct bigint *bi, const int x);
+void bigint_popc(struct bigint *bi);
+struct bigint *bigint_add(const struct bigint *a, const struct bigint *b);
+void bigint_print(struct bigint *bi);
+
