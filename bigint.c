3709d96ce0fdf0f2df2179ba16ccfb6cf0dc71b0
Alvin Chan <altc.alchan@gmail.com>
2024-07-22 21:08:51 -0400

    fix: add missing struct member operators

--- bigint.c
+++ bigint.c
@@ -5,16 +5,16 @@
 #define DEBUG
 
 void bigint_pushc(struct bigint *bi, const int x) {
-	int *tmp_d = realloc(bi, ndigits+sizeof(int);
-	if (!tmp_d) printf("realloc failed to allocate memory; out of memory.");
+	int *tmp_d = realloc(bi, bi->ndigits+sizeof(int));
+	if (!tmp_d) puts("realloc failed to allocate memory; out of memory.");
 	bi->d = tmp_d;
 	bi->ndigits++;
-	bi->d[ndigits-1] = x;
+	bi->d[bi->ndigits-1] = x;
 }
 
 void bigint_popc(struct bigint *bi) {
-	int *tmp_d = realloc(bi, ndigits-sizeof(int));
-	if (!tmp_d) printf("realloc failed to allocate memory; out of memory.");
+	int *tmp_d = realloc(bi, bi->ndigits-sizeof(int));
+	if (!tmp_d) puts("realloc failed to allocate memory; out of memory.");
 	bi->d = tmp_d;
 	bi->ndigits--;
 }
--- bigint.h
+++ bigint.h
@@ -1,5 +1,5 @@
 struct bigint {
 	int ndigits;
 	int *d;
-}
+};
 
