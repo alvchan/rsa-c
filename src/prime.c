db5e0503980a95e80529f33ebf0fc7e4c1fb8117
Alvin Chan <altc.alchan@gmail.com>
2024-08-23 19:42:52 -0400

    Add base prime tester

---
+++ src/prime.c
@@ -0,0 +1,40 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+bool is_prime(int x) {
+	int m;
+	for (int k = 1; k < 10; k++) {
+		if ((x - 1) % (1 << k) == 0)
+			m = (x - 1) >> k;
+		else
+			break;
+	}
+
+	int a = 2;
+
+	long long b = (int) pow(a, m) % x;
+	if (b == 1 || b == x-1)
+		return true;
+
+	for (int i = 0; i < 10; i++) {
+		if (b == 1)
+			return false;
+		else if (b == x-1)
+			return true;
+
+		b = (b*b) % x;
+	}
+
+	return false;
+}
+
+int main(void) {
+	printf("%d\n", is_prime(53));
+	printf("%d\n", is_prime(55));
+	printf("%d\n", is_prime(11));
+	printf("%d\n", is_prime(13));
+	printf("%d\n", is_prime(14));
+	printf("%d\n", is_prime(101));
+}
